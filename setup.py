b6b97ea8154d7f49afb1cab8d8ad57dc920c1637
darachm <4563512-darachm@users.noreply.gitlab.com>
2021-10-13 15:03:24 -0700

    just making style change, and permitting the long description bit that Carsten had suggested but commented out

--- setup.py
+++ setup.py
@@ -4,8 +4,7 @@ setup(
     name='PyFitSeq',
     use_scm_version=True,
     setup_requires=['setuptools_scm'],
-    packages=['PyFitSeq',
-             ],
+    packages=['PyFitSeq'],
     license='MIT',
-    #long_description=open('README.md').read(),
-)
+    long_description=open('README.md').read(),
+    )
